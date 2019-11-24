96e3942e20b0efcf1e0d14bd7686b7d7b28a3b27
heliumface770 <heliumface770@gmail.com>
2019-11-23 20:25:08 -0700

    post request added

--- server.js
+++ server.js
@@ -1,26 +1,35 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-
+const notes = [];
+const note = {};
 const app = express();
 const PORT = 3000;
 
-const json = 
-[{name: "guy",
-  message: "hello",
-  type: "input"
-},
-{name: "gal",
-  message: "bye",
-  type: "input"}]
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
+
+//Routes
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "public/index.html"))
 });
 
 app.get("/notes", function(req, res){
+    res.sendFile(path.join(__dirname, "public/notes.html"))
+});
 
+app.get("/api/notes", function(req, res){
+    return res.json(notes);
+});
+
+
+app.post("/api/notes", function(req, res){
+    var title=req.body.note;
+    var note=req.body.note;
+    console.log("User name = "+title+", password is "+note);
+    res.end("yes");
 });
 
 //Listener
@@ -28,4 +37,5 @@ app.listen(PORT, function(){
     console.log("http://localhost:" + PORT);
 });
 
-// return res.json(json);
+
+// return res.json();
